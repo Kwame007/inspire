eb510875c501c9a1deb8a962392c2bf911289656
Kwame007 <clementnyankuadjei@gmail.com>
2022-03-06 00:29:10 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// request class
+const Request = class {
+  // get method
+  get(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data.slip.advice))
+      .catch((error) => console.log("error occured"));
+  }
+};
+
+// // UI class
+// const UI = class extends Request {};
+// const http = new Request();
+// http.get("https://api.adviceslip.com/advice");
